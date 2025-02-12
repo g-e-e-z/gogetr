150e6914b873a0c9f88ede19269eb66f593808fe
Alex Gordon <gordon112524@gmail.com>
2025-02-11 20:50:18 -0500

    env selector

--- gui/env_selector.go
+++ gui/env_selector.go
@@ -1,21 +1,68 @@
 package gui
 
 import (
-    "github.com/rivo/tview"
-    // "github.com/g-e-e-z/gogetr/config"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/joho/godotenv"
+	"github.com/rivo/tview"
+	// "github.com/g-e-e-z/gogetr/config"
 )
 
-func NewEnvSelector() *tview.InputField {
-    input := tview.NewInputField().
-        SetLabel("Select Environment: ").
-        SetPlaceholder("dev.env, prod.env").
-        SetAcceptanceFunc(func(text string, key rune) bool {
-        // SetAcceptanceFunc(func(text string, key tview.Key) bool {
-            // Logic to load the selected environment file
-            // config.LoadEnvFile(text) // hypothetical function
-            return true
-        })
+type Environment map[string]string
 
-    return input
+type Environments map[string]*Environment
+
+func LoadEnvironments(directory string) (*Environments, error) {
+    environments := make(Environments)
+
+	// Read all files in the specified path
+	files, err := os.ReadDir(directory)
+	if err != nil {
+		return nil, err
+	}
+
+    for _, file := range files {
+        if !file.IsDir() && filepath.Ext(file.Name()) == ".env" {
+            var environment Environment
+            environment, err  := godotenv.Read(filepath.Join(directory, file.Name()))
+            if err != nil {
+                return nil, err
+            }
+            environments[file.Name()] = &environment
+
+        }
+    }
+    return &environments, nil
+
+}
+
+func NewEnvSelector() *tview.TextView {
+	// TODO: This is bad
+	pwd, _ := os.Getwd()
+	requestsDir := filepath.Join(pwd, "requests_dir")
+    fmt.Println("TESTSET", requestsDir)
+	environments, err := LoadEnvironments(requestsDir)
+	if err != nil {
+		log.Panic(err)
+	}
+    environment := tview.NewTextView()//.SetBorder(true).SetTitle("Env and Build Info")
+
+    for k, v := range *environments {
+        fmt.Fprint(environment, k, v,"\n")
+    }
+    // input := tview.NewInputField().
+    //     SetLabel("Select Environment: ").
+    //     SetPlaceholder("dev.env, prod.env").
+    //     SetAcceptanceFunc(func(text string, key rune) bool {
+    //     // SetAcceptanceFunc(func(text string, key tview.Key) bool {
+    //         // Logic to load the selected environment file
+    //         // config.LoadEnvFile(text) // hypothetical function
+    //         return true
+    //     })
+
+    return environment
 }
 
--- gui/gui.go
+++ gui/gui.go
@@ -8,7 +8,8 @@ func RunGUI() {
 	app := tview.NewApplication()
 	flex := tview.NewFlex().
         SetDirection(tview.FlexRow).
-		AddItem(tview.NewBox().SetBorder(true).SetTitle("Env and Build Info"), 4, 1, false).
+		// AddItem(tview.NewBox().SetBorder(true).SetTitle("Env and Build Info"), 4, 1, false).
+        AddItem(NewEnvSelector(), 4, 1, false).
         AddItem(NewRequestSelector(), 0, 4, true)
 	if err := app.SetRoot(flex, true).EnableMouse(true).Run(); err != nil {
 		panic(err)
